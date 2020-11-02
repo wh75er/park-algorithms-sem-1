505979082146e0d195c2850de235aa82c3615faf
wh75er <wh75er@gmail.com>
2020-11-02 03:37:27 +0300

    init interfaces

--- task4/main.cpp
+++ task4/main.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
 
+#define DEFAULT_BUF_SIZE 8
 
+class Array {
+public:
+  Array();
+  ~Array();
+}
+
+class Heap {
+public:
+  Heap();
+  explicit Heap(const int& _arr);
+  ~Heap();
+
+  void insert(int value);
+
+  int extractMax();
+
+  int peekMax() const;
+
+private:
+  int* buffer;
+
+  void buildHeap();
+  void siftDown(int i);
+  void siftUp(int i)
+};
