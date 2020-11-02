f5407e3812b98e987637958bc2e0ed0c94b9cf56
wh75er <wh75er@gmail.com>
2020-11-02 18:36:39 +0300

    Add Heap implementation

--- task4/main.cpp
+++ task4/main.cpp
@@ -1,92 +1,219 @@
+#include <assert.h>
+#include <string.h>
 #include <iostream>
 
-#define PARENT(i) (i-1/2)
+#define PARENT(i) ((i-1)/2)
 #define LEFT(i) (2*i + 1)
 #define RIGHT(i) (2*i + 2)
 
 #define DEFAULT_BUF_SIZE 8
 
-template <typename T>
+bool cmp(int &a, int &b) {
+  if(a <= b) {
+    return true;
+  }
+
+  return false;
+}
+
 class Array {
 public:
   Array() {
     capacity = DEFAULT_BUF_SIZE;
-    buffer = new T[capacity];
+    buffer = new int[capacity];
   }
 
   ~Array() {
-    delete buffer;
+    delete [] buffer;
   }
 
-  T pop_back();
+  void push_back(int value) {
+    if (size == capacity) {
+      resizeBuf();
+    }
 
-  void push_back(T value);
-  
-  T pop_front();
+    buffer[size] = value;
 
-  void push_front(T value);
+    size++;
+  }
 
-  void swap(T &a, T &b) {
-    T temp = a;
+  int pop_back() {
+    if (size == 0) {
+      return -1;
+    }
+
+    int value = buffer[size - 1];
+    size--;
+  }
+
+  void push_front(int value) {
+    if (size == capacity) {
+      resizeBuf();
+    }
+
+    for(size_t i = size; i > 1; i--) {
+      buffer[i] = buffer[i-1];
+    }
+
+    buffer[0] = value;
+
+    size++;
+  }
+
+  int pop_front() {
+    if (size == 0) {
+      return -1;
+    }
+
+    int value = buffer[0];
+
+    for(size_t i = 0; i < size-1; i++) {
+      buffer[i] = buffer[i+1];
+    }
+
+    size--;
+    return value;
+  }
+
+  void copy(const Array& arr) {
+    if(capacity < arr.Size()) {
+      resizeNBuf(arr.Capacity());
+    }
+
+    for(size_t i = 0; i < arr.Size(); i++) {
+      buffer[i] = arr[i];
+    }
+    size = arr.Size();
+  }
+
+  void sliceCopy(const Array& arr, size_t left, size_t right) {
+    assert(right > left);
+
+    if(capacity < right - left) {
+      resizeNBuf(right - left);
+    }
+
+    for(size_t i = 0; i < right - left; i++) {
+      buffer[i] = arr[left + i];
+    }
+    size = right - left;
+  }
+
+  void swap(int &a, int &b) {
+    int temp = a;
     a = b;
     b = temp;
   }
 
-private:
-  resizeBuf() {
+  size_t Size() const {
+    return size;
   }
 
-  T* buffer = nullptr;
+  size_t Capacity() const {
+    return capacity;
+  }
+
+  bool isEmpty() const {
+    if (size == 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  int& operator[](size_t i) const {
+    assert(i < size);
+    return buffer[i];
+  }
+
+private:
+  void resizeNBuf(size_t n) {
+    capacity = n;
+
+    delete [] buffer;
+
+    buffer = new int[capacity];
+  }
+
+  void resizeBuf() {
+    size_t prevCapacity = capacity;
+
+    capacity *= 2;
+    int* temp = new int[capacity];
+
+    memcpy(temp, buffer, size * sizeof(int));
+
+    delete [] buffer;
+
+    buffer = temp;
+  }
+
+  int* buffer = nullptr;
   size_t size = 0;
   size_t capacity = 0;
-}
+};
+
 
 class Heap {
 public:
-  Heap();
-  explicit Heap(const int& _arr);
-  ~Heap();
+  Heap(const Array& _arr, bool (*_cmp)(int&, int&)) : cmp(_cmp) { 
+    arr.copy(_arr);
+    buildHeap();
+  }
 
-  void insert(int value);
+  Heap(const Array& _arr, size_t left, size_t right, bool (*_cmp)(int&, int&)) : cmp(_cmp) { 
+    arr.sliceCopy(_arr, left, right);
+    buildHeap();
+  }
 
-  int extractMax();
+  ~Heap() {
+  }
+
+  void insert(int value) {
+    arr.push_back(value);
+    siftUp(arr.Size() - 1);
+  }
+
+  int extractMax() {
+    assert(!arr.isEmpty());
+
+    int result = arr[0];
+    arr[0] = arr[arr.Size() - 1];
+    arr.pop_back();
+
+    if (!arr.isEmpty()) {
+      siftDown(0);
+    }
+
+    return result;
+  }
 
   int peekMax() const {
-    if (!arr) {
-      return;
-    }
+    assert(!arr.isEmpty());
 
     return arr[0];
   }
 
 private:
-  Array* arr = nullptr;
-
   void buildHeap() {
-    if (!arr) {
-      return;
-    }
+    assert(!arr.isEmpty());
 
-    for(size_t i = PARENT(arr.size() - 1); i > 0; i--) {
+    for(int i = PARENT(arr.Size() - 1); i >= 0; i--) {
       siftDown(i);
     }
   }
 
-  void siftDown(size_t i) {
-    if (!arr) {
-      return;
-    }
-
+  void siftDown(int i) {
     int largest = i;
 
     while(true) {
-      size_t left = LEFT(largest);
-      size_t right = RIGHT(largest);
+      int left = LEFT(largest);
+      int right = RIGHT(largest);
 
-      if (left < arr.size() && arr[left] > largest) {
+      if (left < arr.Size() && !cmp(arr[left], arr[largest])) {
         largest = left;
       }
-      if (right < arr.size() && arr[right] > largest) {
+      if (right < arr.Size() && !cmp(arr[right], arr[largest])) {
         largest = right;
       }
       if (largest != i) {
@@ -98,18 +225,44 @@ private:
     }
   }
 
-  void siftUp(size_t i) {
-    if (!arr) {
-      return;
-    }
-
+  void siftUp(int i) {
     while (i > 0) {
-      size_t parent = PARENT(i);
-      if (arr[i] <= arr[parent]) {
+      int parent = PARENT(i);
+      if (cmp(arr[i], arr[parent])) {
         return;
       }
       arr.swap(arr[i], arr[parent]);
       i = parent;
     }
   }
+
+  Array arr = Array();
+  bool (*cmp)(int&, int&) = nullptr;
 };
+
+void run(std::istream& input, std::ostream& output) {
+  int n = 0;
+  input >> n;
+
+  Array arr = Array();
+  for(int i = 0; i < n; i++) {
+    int value = 0;
+    input >> value;
+
+    arr.push_back(value);
+  }
+
+  int windowSize = 0;
+  input >> windowSize;
+
+  for(int i = 0; i + windowSize <= n; i++) {
+    Heap h = Heap(arr, i, i + windowSize, cmp);
+    output << h.peekMax() << " ";
+  }
+}
+
+int main() {
+  run(std::cin, std::cout);
+
+  return 0;
+}
