9ad90fa7dec6ec2f3a80cc8591bbff7741be9a11
wh75er <wh75er@gmail.com>
2020-12-13 23:16:09 +0300

    working version of hashtable

--- task2-1/main.cpp
+++ task2-1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <cassert>
 
 #define DEFAULT_MAP_SIZE 8
 
@@ -26,8 +28,9 @@ class HashMap {
     Key key;
     bool marked = false;
   };
+
 public:
-  HashMap(Hash hash = Hash()):
+  explicit HashMap(Hash hash = Hash()):
     items_count(0),
     max_load_factor(3.f/4.f),
     hash(hash)
@@ -66,7 +69,7 @@ public:
   }
 
   bool find(const Key& key) {
-    if (map.empty()) {
+    if (!items_count) {
       return false;
     }
 
@@ -74,7 +77,7 @@ public:
 
     size_t i = 1;
 
-    while(pos < map.size() && (!map[pos].key.empty() || map[pos].marked)) {
+    while(!map[pos].key.empty() || map[pos].marked) {
       if(!map[pos].key.empty() && map[pos].key == key) {
         return true;
       }
@@ -121,20 +124,20 @@ private:
 
     std::vector<struct Bucket> new_map(new_map_size);
 
-    for(auto iter : map) {
+    for(auto const &iter : map) {
       if (!iter.key.empty()) {
         size_t pos = hash(iter.key) % new_map_size;
 
         size_t i = 1;
 
-        while(pos < new_map.size()) {
+        while(pos < new_map_size) {
           if(new_map[pos].key.empty()) {
             new_map[pos].key = iter.key;
             new_map[pos].marked = true;
             break;
           }
 
-          pos = probe(pos, i, new_map.size());
+          pos = probe(pos, i, new_map_size);
           i++;
         }
       }
@@ -144,7 +147,7 @@ private:
   }
 
   size_t probe(size_t idx, size_t i, size_t size) {
-    return (idx + i * i) % size;
+    return (idx + i + 1) % size;
   }
 
 
@@ -154,30 +157,110 @@ private:
   Hash hash;
 };
 
-int main() {
+void run(std::istream& input, std::ostream& output) {
   std::string key = "";
   char operation = '\0';
 
   HashMap<std::string> hmap;
-  while(std::cin >> operation >> key) {
-    bool res = false;
+  while(input >> operation >> key) {
+    bool res = true;
     switch (operation) {
       case '+':
-        res = hmap.insert(key);
+        res *= hmap.insert(key);
         break;
       case '-':
-        res = hmap.remove(key);
+        res *= hmap.remove(key);
         break;
       case '?':
-        res = hmap.find(key);
+        res *= hmap.find(key);
         break;
       default:
-        return 1;
+        exit(1);
     }
     if (res) {
-      std::cout << "OK" << std::endl;
+      output << "OK" << std::endl;
     } else {
-      std::cout << "FAIL" << std::endl;
+      output << "FAIL" << std::endl;
     }
   }
+}
+
+void testHashMap() {
+  {
+    std::stringstream input;
+    std::stringstream output;
+    input << "+ h1\n"
+             "+ h2\n"
+             "+ h3\n"
+             "+ h4\n"
+             "+ h5\n"
+             "+ h6\n"
+             "+ h7\n"
+             "+ h8\n"
+             "+ h9\n"
+             "+ h10\n"
+             "+ h11\n"
+             "+ h12\n"
+             "+ h13\n"
+             "+ h14\n"
+             "? h12\n"
+             "- h12\n"
+             "? h12\n";
+
+    run(input, output);
+    std::string ok = "OK\n";
+    std::string fail = "FAIL\n";
+    std::string expectation = "";
+
+    for (auto i = 0; i < 16; i++) {
+      expectation += ok;
+    }
+    expectation += fail;
+
+    assert(output.str() == expectation);
+  }
+  {
+    std::stringstream input;
+    std::stringstream output;
+    input << "+ uivtovnf\n"
+             "+ utmtlrvd\n"
+             "+ alirid\n"
+             "+ jlxlldqkwol\n"
+             "- uivtovnf\n"
+             "- utmtlrvd\n"
+             "- alirid\n"
+             "- jlxlldqkwol\n"
+             "+ fbietxueb\n"
+             "+ yhtg\n"
+             "+ ultdayoiynwxvkb\n"
+             "+ ttddwoaxs\n"
+             "- fbietxueb\n"
+             "- yhtg\n"
+             "- ultdayoiynwxvkb\n"
+             "- ttddwoaxs\n"
+             "+ dqleuopfqnoqvqwpm\n"
+             "+ llyyxchrdwyodyoqei\n"
+             "+ fctocrpxombv\n"
+             "+ aixg\n"
+             "- dqleuopfqnoqvqwpm\n"
+             "- llyyxchrdwyodyoqei\n"
+             "- fctocrpxombv\n"
+             "- aixg";
+
+    run(input, output);
+    std::string ok = "OK\n";
+    std::string fail = "FAIL\n";
+    std::string expectation = "";
+
+    for (auto i = 0; i < 24; i++) {
+      expectation += ok;
+    }
+
+    assert(output.str() == expectation);
+  }
+}
+
+int main() {
+//  run(std::cin, std::cout);
+  testHashMap();
 }
